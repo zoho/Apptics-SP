9498ea036f205a2fd0e720962da37e3aa563e7a9
saravanan-selvam <43129002+saravanan-selvam@users.noreply.github.com>
2021-03-23 12:25:15 +0530

    Added required files

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let version = "1.0.0-alpha"
+let package = Package(
+    name: "Apptics",
+    platforms: [.macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
+    products: [
+        .library(
+            name: "Apptics",
+            targets: ["Apptics"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Apptics",
+            path: "Apptics.xcframework"
+        ),
+        .target(
+            name: "scripts",
+            path:"scripts"
+        )
+    ]
+)
