9fcd7d1283b5596e1614bed32c690c17c9c07a83
saravanan-selvam <43129002+saravanan-selvam@users.noreply.github.com>
2022-03-01 15:47:55 +0530

    The first commit

---
+++ Package.swift
@@ -0,0 +1,86 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let version = "1.1"
+let package = Package(
+    name: "Apptics",
+    defaultLocalization: "en",
+    platforms: [.macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
+    products: [
+        .library(
+            name: "AppticsAnalytics",
+            targets: ["AppticsAnalyticsTarget"]
+        ),
+        .library(
+            name: "AppticsEventTracker",
+            targets: ["AppticsEventTrackerTarget"]
+        ),
+        .library(
+            name: "AppticsScreenTracker",
+            targets: ["AppticsScreenTrackerTarget"]
+        ),
+        .library(
+            name: "AppticsCrashKit",
+            targets: ["AppticsCrashKitTarget"]
+        ),
+        .library(
+            name: "AppticsFeedbackKit",
+            targets: ["AppticsFeedbackKitTarget"]
+        ),
+        .library(
+            name: "AppticsInAppUpdate",
+            targets: ["AppticsInAppUpdateTarget"]
+        ),
+        .library(
+            name: "AppticsRateUs",
+            targets: ["AppticsRateUsTarget"]
+        ),
+        .library(
+            name: "AppticsRemoteConfig",
+            targets: ["AppticsRemoteConfigTarget"]
+        ),
+        .library(
+            name: "AppticsApiTracker",
+            targets: ["AppticsApiTrackerTarget"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppticsAnalyticsTarget",
+            path: "Apptics.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsEventTrackerTarget",
+            path: "AppticsEventTracker.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsScreenTrackerTarget",
+            path: "AppticsScreenTracker.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsCrashKitTarget",
+            path: "AppticsCrashKit.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsFeedbackKitTarget",
+            path: "AppticsFeedbackKit.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsInAppUpdateTarget",
+            path: "AppticsInAppUpdate.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsRateUsTarget",
+            path: "AppticsRateUs.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsRemoteConfigTarget",
+            path: "AppticsRemoteConfig.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppticsApiTrackerTarget",
+            path: "AppticsApiTracker.xcframework"
+        )
+    ]
+)
+
