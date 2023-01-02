26cbd1a5150518b2838006ed3c06d6d916d81e33
saravanavellu <ssaravanan@zohocorp.com>
2023-01-02 17:00:21 +0530

    Updating the Apptics.framework after removing postinstaller file

---
+++ Examples/AppticsDemoiOSApp/AppticsDemo/Test.h
@@ -0,0 +1,20 @@
+//
+//  Test.h
+//  AppticsDemo
+//
+//  Created by Saravanan S on 15/11/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject<UIApplicationDelegate>
+
+-(void) log;
+
+-(void)crash;
+
+@end
+
+NS_ASSUME_NONNULL_END
