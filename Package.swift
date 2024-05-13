5fbb721fdf1ad5e23e47260c9258d8ef07ca7dcf
admin <admin@apptics-test.csez.zohocorpin.com>
2024-05-13 13:04:33 +0530

    published beta 2.0.502

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.0.501"
+let version = "2.0.502"
 let package = Package(
     name: "Apptics",
     defaultLocalization: "en",
