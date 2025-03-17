33d5f098521363973e3e1a9ef2265a620fc732a6
Saravanan <ssaravanan@zohocorp.com>
2025-03-17 16:11:45 +0530

    published 2.2.5 Release  version

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.3
  import PackageDescription
- let version = "2.2.4"
+ let version = "2.2.5"
  let package = Package(
      name: "Apptics",
      defaultLocalization: "en",
